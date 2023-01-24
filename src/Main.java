4d15c6ede6201e6ebc2fa5e5f5e5a4db41cef687
Thege <jonathan.thege@hotmail.com>
2023-01-24 13:19:08 +0100

    Adding calculate file and connecting it to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the Git checkpoint task 1.1");
+        Calculate calculate = new Calculate();
+        Calculate.calculate(23, 5);
     }
 }

9dc7706c713f8224b6809ce622a9c45d894db669
Thege <jonathan.thege@hotmail.com>
2023-01-24 13:12:07 +0100

    Initializing main and check test

---
+++ src/CheckTest.java
@@ -0,0 +1,9 @@
+public class CheckTest {
+    public static void main(String[] args) {
+        System.out.println(returnAnInt(23));
+    }
+
+    public static int returnAnInt (int i) {
+        return i;
+    }
+}
